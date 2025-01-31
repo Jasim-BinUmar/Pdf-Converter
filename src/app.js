b1a522679855d49c1c47610a0eef8cf97947c281
Jasim <jasimbinumar@gmail.com>
2025-01-31 23:10:49 +0500

    Include Pdf manipulation feature merge pdf

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const toPdfRoutes = require('./routes/converterRoutes/toPdfRoutes');
 const fromPdfRoutes = require('./routes/converterRoutes/fromPdfRoutes')
+const pdfManipulationRoutes = require('./routes/pdfManipulationRoutes')
 const app = express();
 
 app.use(cors());
@@ -11,7 +12,10 @@ app.use(bodyParser.json());
 // To Pdf Conversion Routes
 app.use('/api/converter/toPdf', toPdfRoutes);
 
-// From Pdf conversion Routes
+// From Pdf Conversion Routes
 app.use('/api/converter/fromPdf', fromPdfRoutes);
+
+// Pdf Manipulation Routes
+app.use('/api/manipulation/', pdfManipulationRoutes);
 
 module.exports = app;
